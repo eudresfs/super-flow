014214765e592a272e96e54ec0eab1677bab4e38
Glitch (whatsapp-flows-appointment) <none>
2024-06-20 20:21:26 +0000

    🐯🚈 Checkpoint ./src/flow.js:59765425/11759

--- src/flow.js
+++ src/flow.js
@@ -8,90 +8,90 @@
 // this object is generated from Flow Builder under "..." > Endpoint > Snippets > Responses
 // To navigate to a screen, return the corresponding response from the endpoint. Make sure the response is enccrypted.
 const SCREEN_RESPONSES = {
-    LOAN: {
-        "version": "3.0",
-        "screen": "LOAN",
-        "data": {
-            "tenure": [
-                {
-                    "id": "months12",
-                    "title": "12 months"
-                },
-                {
-                    "id": "months24",
-                    "title": "24 months"
-                },
-                {
-                    "id": "months36",
-                    "title": "36 months"
-                },
-                {
-                    "id": "months48",
-                    "title": "48 months"
-                }
-            ],
-            "amount": [
-                {
-                    "id": "amount1",
-                    "title": "\u20b9 7,20,000"
-                },
-                {
-                    "id": "amount2",
-                    "title": "\u20b9 3,20,000"
-                },
-                {
-                    "id": "amount3",
-                    "title": "\u20b9 9,20,000"
-                },
-                {
-                    "id": "amount4",
-                    "title": "\u20b9 1,20,000"
-                }
-            ],
-            "emi": "\u20b9 20,000",
-            "rate": "9% pa",
-            "fee": "500",
-            "selected_amount": "amount1",
-            "selected_tenure": "months48"
-        }
+  LOAN: {
+    version: "3.0",
+    screen: "LOAN",
+    data: {
+      tenure: [
+        {
+          id: "months12",
+          title: "12 months",
+        },
+        {
+          id: "months24",
+          title: "24 months",
+        },
+        {
+          id: "months36",
+          title: "36 months",
+        },
+        {
+          id: "months48",
+          title: "48 months",
+        },
+      ],
+      amount: [
+        {
+          id: "amount1",
+          title: "\u20b9 7,20,000",
+        },
+        {
+          id: "amount2",
+          title: "\u20b9 3,20,000",
+        },
+        {
+          id: "amount3",
+          title: "\u20b9 9,20,000",
+        },
+        {
+          id: "amount4",
+          title: "\u20b9 1,20,000",
+        },
+      ],
+      emi: "\u20b9 20,000",
+      rate: "9% pa",
+      fee: "500",
+      selected_amount: "amount1",
+      selected_tenure: "months48",
     },
-    DETAILS: {
-        "version": "3.0",
-        "screen": "DETAILS",
-        "data": {
-            "is_upi": false,
-            "is_account": false
-        }
+  },
+  DETAILS: {
+    version: "3.0",
+    screen: "DETAILS",
+    data: {
+      is_upi: false,
+      is_account: false,
     },
-    SUMMARY: {
-        "version": "3.0",
-        "screen": "SUMMARY",
-        "data": {
-            "amount": "\u20b9 7,20,000",
-            "tenure": "12 months",
-            "rate": "12 months",
-            "emi": "12 months",
-            "fee": "12 months",
-            "payment_mode": "12 months"
-        }
+  },
+  SUMMARY: {
+    version: "3.0",
+    screen: "SUMMARY",
+    data: {
+      amount: "\u20b9 7,20,000",
+      tenure: "12 months",
+      rate: "12 months",
+      emi: "12 months",
+      fee: "12 months",
+      payment_mode: "12 months",
     },
-    COMPLETE: {
-        "version": "3.0",
-        "screen": "COMPLETE",
-        "data": {}
+  },
+  COMPLETE: {
+    version: "3.0",
+    screen: "COMPLETE",
+    data: {},
+  },
+  SUCCESS: {
+    version: "3.0",
+    screen: "SUCCESS",
+    data: {
+      extension_message_response: {
+        params: {
+          flow_token: "REPLACE_FLOW_TOKEN",
+          some_param_name: "PASS_CUSTOM_VALUE",
+        },
+      },
     },
-    SUCCESS: {
-        "version": "3.0",
-        "screen": "SUCCESS",
-        "data": {
-            "extension_message_response": {
-                "params": {
-                    "flow_token": "REPLACE_FLOW_TOKEN",
-                    "some_param_name": "PASS_CUSTOM_VALUE"
-                }
-            }
-        }
-    },
+  },
 };
 
 const LOAN_OPTIONS = {
@@ -163,17 +163,37 @@ export const getNextScreen = async (decryptedBody) => {
     switch (screen) {
       // handles when user interacts with LOAN screen
       case "LOAN":
-        // 
-        if(data.ammount === null && data.tenure === null) {
+        // Handles user clicking on Continue to navigate to next screen
+        if (data.ammount == null && data.tenure == null) {
           return {
-            ...SCREEN_RESPONSES.DETAILS
-          }
+            ...SCREEN_RESPONSES.DETAILS,
+          };
         }
-        // update the loan quote based on user selected tenure
+        // otherwise refresh quote based on user selection
+        // TODO update the loan quote based on user selected tenure
         return {
           ...SCREEN_RESPONSES.LOAN,
           data: {
-            ...LOAN_OPTIONS[data.tenure]
+            ...LOAN_OPTIONS[data.tenure],
+          },
+        };
+      case "DETAILS":
+        // Handles user selecting UPI or Banking selector
+        if (data.payment_mode != null) {
+          return {
+            ...SCREEN_RESPONSES.DETAILS,
+            data: {
+              is_upi: data.payment_mode == 'UPI',
+              is_account: data.payment_mode == 'Bank',
+            },
+          };
+        }
+        // Handles user clicking on Continue
+        // Store user provide disburesment details
+        return {
+          ...SCREEN_RESPONSES.SUMMARY,
+          data: {
+            ...LOAN_OPTIONS[data.tenure],
           },
         };
 
